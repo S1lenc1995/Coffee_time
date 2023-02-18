44dba2be396862d5f7c409a3a4d4d433ce4e12b1
Bohdan Kletskyi <underwater3499@gmail.com>
2023-02-18 09:15:30 +0200

    d

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter as Router } from 'react-router-dom';
+import {BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import CoffeeMachineProvider from './context';
 
@@ -16,11 +16,11 @@ root.render(
   <Provider store={store}>
    
       <CoffeeMachineProvider>  
-      <Router>
+      <BrowserRouter>
      
           <App />
        
-      </Router>
+      </BrowserRouter>
         </CoffeeMachineProvider>  
  
   </Provider>
